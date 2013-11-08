a0bf89cccddc2b5b0e37f6a6169f705ce9ddfbca
glumoff <glumoff@glum>
2013-11-08 03:25:23 +0200

    + login form + translation support begin according to user locale

--- Symfony/src/Big/ElibreBundle/Controller/DefaultController.php
+++ Symfony/src/Big/ElibreBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //require_once '/mnt/hd/work/www/elibre/Symfony/src/Big/ElibreBundle/db/ElibreDBDelegate.php';
 //require_once '../Model/Theme.php';
 use Big\ElibreBundle\db\ElibreDBDelegate;
+
 //use Big\ElibreBundle\Model\Theme;
 
 class DefaultController extends Controller {
@@ -18,6 +19,8 @@ class DefaultController extends Controller {
   }
 
   protected function getTemplateParams() {
+    $this->get('translator')->setLocale($this->getRequest()->getPreferredLanguage());
+
     if ($this->templateParams === NULL) {
       $this->templateParams = array();
       $this->templateParams['menuThemes'] = $this->getThemesMenuArray();
---
+++ Symfony/src/Big/ElibreBundle/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Description of SecurityController
+ *
+ * @author Alexander Glumoff <glumoff at gmail.com>
+ */
+
+namespace Big\ElibreBundle\Controller;
+
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Intl\Locale;
+
+class SecurityController extends DefaultController {
+
+  public function loginAction() {
+    return $this->render('BigElibreBundle:Default:login.html.twig', $this->getTemplateParams());
+  }
+
+  protected function getTemplateParams() {
+    parent::getTemplateParams();
+
+    $request = $this->getRequest();
+    $session = $request->getSession();
+    if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    }
+    else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+      $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+    }
+
+    $error_msg = '';
+//    echo get_class($error);
+    if (is_a($error, 'Exception')) {
+      $error_msg = $error->getMessageKey();
+    }
+  
+    $this->templateParams['last_username'] = $session->get(SecurityContext::LAST_USERNAME);
+    $this->templateParams['error_msg'] = $this->get('translator')->trans($error_msg, array(), 'security');
+
+    return $this->templateParams;
+  }
+
+}
